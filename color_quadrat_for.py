7db4e4d8281a76a67956de5fcfa9dff8d793f79b
unaicervantes <unaicervantes@gmail.com>
2021-10-20 20:43:36 +0200

    color quadrat validacio dades

--- color_quadrat_for.py
+++ color_quadrat_for.py
@@ -12,13 +12,25 @@
 __author__ = "Unai Cervantes"
 
 import sys
+if len(sys.argv) > 2:
+    print("Les cel·les tenen dos valors.")
+    quit()
+
 posicion = (sys.argv[1])
 valor1 = posicion[0]
-valor2 = int(posicion[1])
+valor2 = int(posicion[1::])
 #print(valor1,valor2)
 color = None
 letras = ("a","b","c","d","e","f","g","h")
 dicc = {"a":"Negro","b":"Blanco","c":"Negro","d":"Blanco","e":"Negro","f":"Blanco","g":"Negro","h":"Blanco"}
+#Validació de dades
+if valor1 not in letras:
+    print("Posició invalida (Les columnes són de la a a la h)")
+    quit()
+if valor2 > 8:
+    print("Posició invalida (Només hi ha 8 files)")
+    quit()
+#Obtenció del color de les columnes
 while True:
     for lista_letras in letras:
         
@@ -26,7 +38,7 @@ while True:
             #print (dicc[lista_letras])
             color = (dicc[lista_letras])
     break
-
+#Obtenció del color de les files i del resultat
 if valor2 %2 == 0:
     if color=="Blanco":
         color="Negro"

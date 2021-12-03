aea7b0d87824ff587757116e99f522b4c0d90539
unaicervantes <unaiervantes@gmail.com>
2021-12-03 17:14:13 +0100

    quadrat v2

--- Estructures/Exercicis/quadrat.py
+++ Estructures/Exercicis/quadrat.py
@@ -2,9 +2,12 @@
 # _*_ coding: utf-8 _*_
 import sys
 n = int(sys.argv[1])
-
-for quadrat in range(n):
-    total = ""
-    for resultat in range(0,n+1,1):       
-        total += str(resultat)+" "
-    print(total) 
+contador = 0
+while contador != n:
+    for quadrat in range(n):
+        total = ""
+        for resultat in range(0,n+1,1):       
+            total += str(resultat)+" "
+        print(total) 
+    print("---------")
+    contador += 1
